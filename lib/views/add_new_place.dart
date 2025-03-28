0b56e9cc2b6379f65c1c1898b0ff963aa037cee3
Othmar Gispert <ogispert@gmail.com>
2025-03-28 17:45:24 -0500

    Adding location

--- lib/views/add_new_place.dart
+++ lib/views/add_new_place.dart
@@ -5,6 +5,8 @@ import 'package:places/providers/places_provider.dart';
 import 'package:places/widgets/image_input.dart';
 import 'dart:io';
 
+import 'package:places/widgets/location_input.dart';
+
 class AddNewPlace extends ConsumerStatefulWidget {
   const AddNewPlace({super.key});
 
@@ -18,7 +20,12 @@ class _AddNewPlaceState extends ConsumerState<AddNewPlace> {
   File? selectedImage;
 
   void savePlace() {
-    if (formKey.currentState?.validate() == true && selectedImage != null) {
+    if (selectedImage == null) {
+      // show an alert
+      return;
+    }
+
+    if (formKey.currentState?.validate() == true) {
       formKey.currentState?.save();
 
       final newPlace = Place(name: enteredPlaceName, image: selectedImage!);
@@ -35,56 +42,60 @@ class _AddNewPlaceState extends ConsumerState<AddNewPlace> {
       appBar: AppBar(title: Text('Add New Place')),
       body: Form(
         key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextFormField(
-                maxLength: 50,
-                decoration: const InputDecoration(labelText: 'Place name'),
-                onTapOutside: (PointerDownEvent event) {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                },
-                keyboardType: TextInputType.name,
-                style: const TextStyle(color: Colors.white),
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
-                    return 'Name must be between 1 and 50 characters.';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  enteredPlaceName = value ?? "";
-                },
-              ),
-              SizedBox(height: 16),
-              ImageInput(
-                onPickImage: (image) {
-                  selectedImage = image;
-                },
-              ),
-              SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      formKey.currentState?.reset();
-                    },
-                    child: const Text('Reset'),
-                  ),
-                  SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: savePlace,
-                    icon: Icon(Icons.add),
-                    label: const Text('Save Place'),
-                  ),
-                ],
-              ),
-            ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(labelText: 'Place name'),
+                  onTapOutside: (PointerDownEvent event) {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  keyboardType: TextInputType.name,
+                  style: const TextStyle(color: Colors.white),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Name must be between 1 and 50 characters.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    enteredPlaceName = value ?? "";
+                  },
+                ),
+                SizedBox(height: 16),
+                ImageInput(
+                  onPickImage: (image) {
+                    selectedImage = image;
+                  },
+                ),
+                SizedBox(height: 16),
+                LocationInput(),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        formKey.currentState?.reset();
+                      },
+                      child: const Text('Reset'),
+                    ),
+                    SizedBox(width: 16),
+                    ElevatedButton.icon(
+                      onPressed: savePlace,
+                      icon: Icon(Icons.add),
+                      label: const Text('Save Place'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
